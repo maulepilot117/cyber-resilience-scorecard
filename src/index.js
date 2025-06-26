c0a4fd41405f83f0608fe1067c79cc713f2138e0
maulepilot117 <chris.white@rubrik.com>
2025-06-26 08:54:40 -0400

    all the html stuff

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CyberResilienceScorecard from './CyberResilienceScorecard';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <CyberResilienceScorecard />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx}', './public/index.html'],
+  darkMode: false,
+  theme: { extend: {} },
+  variants: { extend: {} },
+  plugins: []
+};
